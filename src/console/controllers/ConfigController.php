0092fbf0e1499e9857de1a8a9636b2939d406aa8
Jordan Beattie <beattiejo1@gmail.com>
2024-01-18 09:31:05 +0000

    Updated config command

--- src/console/controllers/ConfigController.php
+++ src/console/controllers/ConfigController.php
@@ -22,16 +22,19 @@ class ConfigController extends Controller
         $destination = Craft::$app->getPath()->getConfigPath() . '/cookie-consent.php'; 
 
         /* Copy template to config directory */
-        if( file_exists( $source ) )
+        if( $this->hasSourceFile() )
         {
-            if( !file_exists( $destination ) )
+            if( !$this->hasDestinationFile() )
             {
-                copy( $source, $destination );
-                $this->line( "Config file published to config/cookie-consent.php", "success" );
+                $this->publishFile();
             }
             else
             {
                 $this->line( "Config file already exists.", "error" );
+                if( $this->confirm("Do you want to overwrite it?") )
+                {
+                    $this->publishFile();
+                }
             }
         }
         else
@@ -81,6 +84,53 @@ class ConfigController extends Controller
     private function end()
     {
         return ExitCode::OK;
+    }
+
+    /*
+     * Publish File
+     */
+    private function publishFile()
+    {
+        if( copy( $this->getSourceFile(), $this->getDestinationFile() ) )
+        {
+            $this->line( "Config file published to config/cookie-consent.php", "success" );
+        }
+        else
+        {
+            $this->line( "There was an unknown error publishing the config file", "error" );
+        }
+    }
+
+    /*
+     * Get Source
+     */
+    private function getSourceFile()
+    {
+        return __DIR__ . "/../../config/cookie-consent.php";
+    }
+
+    /*
+     * Get Destination
+     */
+    private function getDestinationFile()
+    {
+        return Craft::$app->getPath()->getConfigPath() . '/cookie-consent.php';
+    }
+
+    /*
+     * Has Source File
+     */
+    private function hasSourceFile()
+    {
+        return file_exists( $this->getSourceFile() );
+    }
+
+    /*
+     * Has Destination File
+     */
+    private function hasDestinationFile()
+    {
+        return file_exists( $this->getDestinationFile() );
     }
     
 }
